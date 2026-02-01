6ef7598687e39996f8f110475a925d11047a0bef
satyanarayana <pillastnrn@gmail.com>
2026-02-01 23:22:09 +0530

    frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://secure-file-upload-system-m6jq.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((req) => {
